77bd2ba1f919f7542efc9d49f30ded7ee813243b
Karn Kamolnavin <karn.lattecandy@gmail.com>
2026-03-02 12:05:33 +0700

    Test

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,3 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
+
+export const prerender = true;
--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,16 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		// adapter-static requires prerendering all pages or ensuring a fallback page exists
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html',
+			precompress: false,
+			strict: true
+		})
 	}
 };
 
